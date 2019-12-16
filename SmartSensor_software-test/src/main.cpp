c0397b1e9da8303cbc99c1c74d022ef3107a9b2a
Michel Megens <michel@michelmegens.net>
2019-12-16 20:21:03 +0100

    main: update device initialisation
    Add device driver initialisation.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SmartSensor_software-test/src/main.cpp
+++ SmartSensor_software-test/src/main.cpp
@@ -12,10 +12,20 @@
 #include <XBee.h>
 #include <Adafruit_VEML7700.h>
 
+static HDC1080 rh;
+static Adafruit_VEML7700 light_sensor;
+
+static constexpr uint8_t HDC1080_I2C_ADDR = 0x40;
+
 void loop()
 {
 }
 
 void setup()
 {
+	rh.begin(HDC1080_I2C_ADDR);
+	light_sensor.begin();
+
+	light_sensor.setGain(VEML7700_GAIN_1);
+	light_sensor.setIntegrationTime(VEML7700_IT_800MS);
 }
