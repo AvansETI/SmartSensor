c368d8c65b4358f92c988dedef67aefe254e5173
Maurice Snoeren <mac.snoeren@avans.nl>
2019-12-16 22:23:07 +0100

    Added definitions for the pinout of the smartsensor and impl. setup.

---
+++ SmartSensor_software-test/include/SmartSensor.h
@@ -0,0 +1,39 @@
+/* SmartSensor.h
+   Defines to map the pins and hardware of the SmartSensor.
+*/
+
+#define VBAT_ADC0 PINA0
+#define HAT_AIO1 PINA1
+#define INTA_ADC3 PINA2
+#define HAT_AIO2 PINA3
+#define HAT_AIO6 PINA4
+#define HAT_AIO4 PINA5
+#define HAT_AIO5 PINA6
+#define HAT_AIO3 PINA7
+
+#define HDC1080_IO6 PINB0
+#define HAT_DIO5 PINB1
+#define HAT_DIO3 PINB2
+#define HAT_DIO4 PINB3
+#define SPI_SS PINB4
+#define SPI_MOSI PINB5
+#define SPI_MISO PINB6
+#define SPI_SCK PINB7
+
+#define I2C_SCL PINC0
+#define I2C_SDA PINC1
+#define LED_IO8 PINC2
+#define Vadap_IO9 PINC3
+#define XBEE_SLEEP PINC4
+#define HAT_DIO1 PINC5
+#define HAT_DIO6 PINC6
+#define HAT_DIO7 PINC7
+
+#define AVR_DBG_RX PIND0
+#define AVR_DBG_TX PIND1
+#define XBEE_ON_OFF PIND2
+#define AVR_RX PIND3
+#define AVR_TX PIND4
+#define VEML7700_IO5 PIND5
+#define HAT_DIO2 PIND6
+#define HAT_DIO31 PIND7
--- SmartSensor_software-test/src/main.cpp
+++ SmartSensor_software-test/src/main.cpp
@@ -1,8 +1,10 @@
 /*
- * Entry point.
+ * SmartSensor main
  * 
  * @author Michel Megens
  * @email  michel@michelmegens.net
+ * @author Maurice Snoeren
+ * @email  mac.snoeren@avans.nl
  */
 
 #include <stdlib.h>
@@ -11,11 +13,69 @@
 #include <HDC1080.h>
 #include <XBee.h>
 #include <Adafruit_VEML7700.h>
+#include <SmartSensor.h>
 
-void loop()
-{
+void loop() {
 }
 
-void setup()
-{
+// Method that tests all the element on the board.
+void test() {
+
+}
+
+// Set up the pins of your HAT module here!
+// Default everything is set to INPUT.
+void setupHAT() {
+    pinMode(HAT_AIO1, INPUT);
+    pinMode(HAT_AIO2, INPUT);
+    pinMode(HAT_AIO3, INPUT);
+    pinMode(HAT_AIO4, INPUT);
+    pinMode(HAT_AIO5, INPUT);
+    pinMode(HAT_AIO6, INPUT);
+
+    pinMode(HAT_DIO1, INPUT);
+    pinMode(HAT_DIO2, INPUT);
+    pinMode(HAT_DIO3, INPUT);
+    pinMode(HAT_DIO4, INPUT);
+    pinMode(HAT_DIO5, INPUT);
+    pinMode(HAT_DIO6, INPUT);
+    pinMode(HAT_DIO7, INPUT);
+    pinMode(HAT_DIO31, INPUT);
+}
+
+// Configure the Atmega according the hardware design!
+void setup() {
+    pinMode(VBAT_ADC0, INPUT);  //Battery measurement
+    pinMode(INTA_ADC3, OUTPUT); // Realtime clock: /INTA
+    //digitalWrite(INTA_ADC3, LOW); // ??
+
+    pinMode(HDC1080_IO6, OUTPUT);   // HDC1080 Humidity/Temp On/Off
+    digitalWrite(HDC1080_IO6, LOW); // Switch HDC1080 default off
+
+    pinMode(SPI_SS, OUTPUT); // SS
+    pinMode(SPI_MOSI, OUTPUT); // MOSI
+    pinMode(SPI_MISO, INPUT);  // MISO
+    pinMode(SPI_SCK, OUTPUT); // SCK
+
+    pinMode(I2C_SCL, OUTPUT);
+    pinMode(I2C_SDA, OUTPUT);
+    pinMode(LED_IO8, OUTPUT);
+    pinMode(Vadap_IO9, INPUT); // When adapter is plugged in 5V (Does it work?)
+    
+    pinMode(XBEE_SLEEP, OUTPUT);
+    digitalWrite(XBEE_SLEEP, LOW); // ?? Check XBee documentation
+
+    pinMode(AVR_DBG_RX, INPUT); // The real UART!!
+    pinMode(AVR_DBG_TX, OUTPUT);
+    
+    pinMode(XBEE_ON_OFF, OUTPUT); // XBee module
+    digitalWrite(XBEE_ON_OFF, LOW);
+
+    pinMode(AVR_RX, INPUT); // Soft UART??
+    pinMode(AVR_TX, OUTPUT); // ??
+
+    pinMode(VEML7700_IO5, OUTPUT); // Light sensor
+    digitalWrite(VEML7700_IO5, LOW);
+
+    setupHAT();
 }
