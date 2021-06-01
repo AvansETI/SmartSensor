56bc2b01ce0e969ea80743000027973eea0dac74
Maurice Snoeren <mac.snoeren@avans.nl>
2021-06-01 11:18:43 +0200

    Added token

--- SmartNetwork/python/main.py
+++ SmartNetwork/python/main.py
@@ -4,7 +4,7 @@ from smartnetwork.smartnetwork import SmartNetwork
 
 host = "http://localhost:8086"
 org = "smartsensor"
-token = "ZqJdkWyrYtrfkybw1yBzA5p8eS6NcjfyIcT0PJ_R4j9N-NNbEAoMn-K_Ie95tM-61lTJ-wn89oaAD7AjfJvY3A=="
+token = "zkrK26xfHz9rvGY7rBqcvPadvgwojmtLgcv7Cuc0dlpNUMs9fhlBnsoi2oBHdmMDY0C7V-s90Clb5ApUIEmxvQ=="#ZqJdkWyrYtrfkybw1yBzA5p8eS6NcjfyIcT0PJ_R4j9N-NNbEAoMn-K_Ie95tM-61lTJ-wn89oaAD7AjfJvY3A=="
 
 smartnetwork = SmartNetwork(host, org, token)
 
@@ -17,7 +17,9 @@ smartnetwork.delete_all_node_alerts()
 
 print(smartnetwork)
 
-time.sleep(10)
+while (1==1):
+    time.sleep(10)
+
 
 smartnetwork.stop()
 
