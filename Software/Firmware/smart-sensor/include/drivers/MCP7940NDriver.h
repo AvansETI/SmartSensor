356abe557068cef5993b41c9b33539e59ce47978
macsnoeren <macsnoeren@gmail.com>
2021-09-11 16:27:09 +0200

    Added a template for the MCP7940Driver to add the RTC to the play.

---
+++ Software/Firmware/smart-sensor/include/drivers/MCP7940NDriver.h
@@ -0,0 +1,97 @@
+#pragma once
+/**
+ * @file       : MCP7940NDriver.h
+ * @description: This driver handles all the aspects of the MCP7940N chip from Sensiron that is connected to the I2C(0) bus. The
+ *               chip is able to hold the real date and time. When the power is shut down, this chip maintains the real date for
+ *               about 5 days (calculated) using an 1F super cap. Furthermore, the MFP pin (interrupt) of the chip is connected
+ *               to PB2 pin of the Atmega.
+                 http://ww1.microchip.com/downloads/en/devicedoc/20005010f.pdf
+ * @date       : 10-09-2021
+ * @author     : Maurice Snoeren (MS)
+ * @version    : 0.1
+ * @todo       : The loop contains blocking function calls!
+ * @updates
+ * 
+ */
+#include <stdint.h>
+#include <drivers/Driver.h>
+
+#include <board-support/drivers/TWIDriver2.h>
+
+/* Define how the MFP pin is connected to the Atmega. */
+#define MCP7940_MFP_PIN PB2;
+#define MCP7940_MFP_PORT PORTB;
+
+/* Address of the MCP7940N chip on the I2C bus */
+constexpr uint8_t MCP7940_I2C_ADDRESS = 0xDE;
+
+// MS: Must be moved to a more generic file!
+struct RTCTimestamp {
+  uint8_t seconds;
+  uint8_t minutes;
+  uint8_t hours;
+  uint8_t weekday;
+  uint8_t date;
+  uint8_t month;
+  uint8_t year;
+};
+
+/* for the setup
+bit 4 PWRFAIL: Power Failure Status bit(1,2)
+1 = Primary power was lost and the power-fail time-stamp registers have been loaded (must be
+cleared in software). Clearing this bit resets the power-fail time-stamp registers to ‘0’.
+0 = Primary power has not been lost
+bit 3 VBATEN: External Battery Backup Supply (VBAT) Enable bit
+1 = VBAT input is enabled
+0 = VBAT input is disabled
+*/
+
+class MCP7940Driver: public Driver {
+public:
+
+    int setup();
+    int loop();
+    int reset();
+    int sleep();
+    int wakeup();
+    
+    /* Check wheter the chip is connected and responding to I2C requests. */
+    bool isConnected();
+
+    /* Add POWER-DOWN/POWER-UP TIME-STAMP */
+    RTCTimestamp getPowerDownTimestamp();
+
+    /* Returns the current time from the chip. */
+    RTCTimestamp getTime();
+
+    /* Set the time based on a RTCTimestamp instance of the chip. */
+    void setTime(const RTCTimestamp &t);
+
+    /* Set the time based on an ISO8601 string of the chip. */
+    void setTime(const char* iso8601);
+
+    // Add enableAlarm function to set the alarm and interrupt
+    // Add disableAlarm function to unset the alarm and interrupt
+    // Set callback eventAlarm function using an interface class RTCAlarmEvent(), it is called when the alarm flag is enable
+    //   => Set the option to use interrupts of callback function.
+    
+    /*The MCP7940N has 64 bytes of SRAM for general purpose usage. It is retained when the primary power
+supply is removed if a backup supply is present and
+enabled.
+Although the SRAM is a separate block from the RTCC
+registers, they are accessed using the same control
+byte, ‘1101111X’.*/
+    // Add setByte, getByte to use the SRAM of the chip, storing things to remember?
+
+
+
+    // bool startClock(); // OLD?
+    // bool stopClock();  // OLD?
+
+    /* Helper function to convert the RTCTimestamp to an ISO8601 string. */
+    static const char* convertToISO8601(const RTCTimestamp &t);
+
+    /* Helper function to convert an ISO8601 string to a RTCTimestamp. */
+    static const RTCTimestamp convertFromISO8601(const char* iso8601);
+};
+
