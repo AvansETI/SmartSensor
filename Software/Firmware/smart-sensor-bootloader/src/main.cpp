c62776e0ec0cccf79f03cbc5fe08851c2c35be3a
Tom <tomkaas74@gmail.com>
2021-11-11 11:24:52 +0100

    Added offset address

--- Software/Firmware/smart-sensor-bootloader/src/main.cpp
+++ Software/Firmware/smart-sensor-bootloader/src/main.cpp
@@ -14,6 +14,7 @@
 #include <avr/pgmspace.h>
 
 char offbuffer[2];
+bool jumpcommand = false;
 
 /* When a character is received on the serial bus, this interrupt is called. */
 ISR(USART0_RX_vect) {
@@ -38,6 +39,8 @@ ISR(USART0_RX_vect) {
 				
 				offbuffer[0] = 'P';
 				offbuffer[1] = 'W';
+
+                jumpcommand = true;
 			} else {
 				offbuffer[1] = c;
 			}
@@ -63,6 +66,44 @@ ISR(USART0_RX_vect) {
 				UDR0 = c;
 		while (!( UCSR0A & (1<<UDRE)));
 				UDR0 = '\n\r';
+}
+
+void boot_program_page (uint32_t page, uint8_t *buf)
+{
+	uint16_t i;
+	uint8_t sreg;
+
+	// Disable interrupts.
+
+	sreg = SREG;
+	cli();
+
+	eeprom_busy_wait ();
+
+	boot_page_erase (page);
+	boot_spm_busy_wait ();      // Wait until the memory is erased.
+
+	for (i=0; i<SPM_PAGESIZE; i+=2)
+	{
+		// Set up little-endian word.
+
+		uint16_t w = *buf++;
+		w += (*buf++) << 8;
+
+		boot_page_fill (page + i, w);
+	}
+
+	boot_page_write (page);     // Store buffer in flash page.
+	boot_spm_busy_wait();       // Wait until the memory is written.
+
+	// Reenable RWW-section again. We need this if we want to jump back
+	// to the application after bootloading.
+
+	boot_rww_enable ();
+
+	// Re-enable interrupts (if they were ever enabled).
+
+	SREG = sreg;
 }
 
 int main(void) {
@@ -100,6 +141,23 @@ int main(void) {
 			UDR0 = '\r';
 
 		_delay_ms(100);
+
+        if (jumpcommand)
+        {
+            int i = 0;
+				unsigned char shutdown[] = "Jumping to other program. \n";
+				while (shutdown[i] != 0)
+				{
+					while (!(UCSR0A & (1<<UDRE)));
+					{
+						UDR0 = shutdown[i];
+						i++;
+					}
+					
+				}
+            asm("jmp 0");
+        }
+        
 		
 		
 	}
