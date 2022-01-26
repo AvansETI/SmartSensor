b453e6d7736ec6a84ad492430f39ab8daadc9550
Tom <tomkaas74@gmail.com>
2022-01-26 19:26:28 +0100

    Issue determined to lie in fuses

--- Software/Firmware/smart-sensor-bootloader/src/main.cpp
+++ Software/Firmware/smart-sensor-bootloader/src/main.cpp
@@ -555,7 +555,7 @@ int main(void)
 	PORTD &= ~(1 << PD4);
 	_delay_ms(1000);
 
-	
+
 	timesincechar = 0;
 	uint32_t baudrate = 9600;
 	uint32_t ubrr = 20000000 / 16 / 9600 - 1; //((20000000 -((baudrate) * 8L)) / ((baudrate) * 16UL));
