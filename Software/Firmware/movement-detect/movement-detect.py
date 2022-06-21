4e92be62516c2e23a59bac146ad3d387fcccb5fa
Timboooh <th.vansark@student.avans.nl>
2022-06-21 11:23:55 +0200

    yo

--- Software/Firmware/movement-detect/movement-detect.py
+++ Software/Firmware/movement-detect/movement-detect.py
@@ -2,74 +2,65 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-CAPTURE_DELAY_MS = 10_000
-
-ITERATIONS = 5
-ITERATION_DELAY_MS = 1000
+CAPTURE_DELAY_MS = 1_000
 
 cap = cv2.VideoCapture(1)
 
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 192) 
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 108)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920) 
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 previous_frame = None    
 
-
 while True:
-    frames = []
+
+    # 1. Load image; convert to RGB
     
-    for i in range(ITERATIONS):
-        ret, frame = cap.read()
-  
-        prepared_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        prepared_frame = cv2.GaussianBlur(src=prepared_frame, ksize=(5, 5), sigmaX=0)
-        frames.append(prepared_frame)
+    ret, img_rgb = cap.read()
 
-        print("Captured frame:", i)
-        cv2.waitKey(ITERATION_DELAY_MS)
-               
+
+    # 2. Prepare image; grayscale and blur
+    prepared_frame = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    prepared_frame = cv2.GaussianBlur(src=prepared_frame, ksize=(5, 5), sigmaX=0)
+
+    # 2. Calculate the difference
+    if (previous_frame is None):
+      # First frame; there is no previous one yet
+      previous_frame = prepared_frame
+      continue
+
+    # 3. Set previous frame and continue if there is None
+    if (previous_frame is None):
+      # First frame; there is no previous one yet
+      previous_frame = prepared_frame
+      continue
+
     # calculate difference and update previous frame
-    diff_frame0_1 = cv2.absdiff(src1=frames[0], src2=frames[1])
-    diff_frame1_2 = cv2.absdiff(src1=frames[1], src2=frames[2])
-    diff_frame2_3 = cv2.absdiff(src1=frames[2], src2=frames[3])
-    diff_frame3_4 = cv2.absdiff(src1=frames[3], src2=frames[4])
-
-    # we imagine the matrix is overflow/saturation protected
-    diff_full = diff_frame0_1 + diff_frame1_2 + diff_frame2_3 + diff_frame3_4 
-
-    cv2.imshow('diff_frame0_1', diff_frame0_1)
-    cv2.imshow('diff_frame1_2', diff_frame1_2)
-    cv2.imshow('diff_frame2_3', diff_frame2_3)
-    cv2.imshow('diff_frame3_4', diff_frame3_4)
-
-    cv2.imshow('diff_full', diff_full)
+    diff_frame = cv2.absdiff(src1=previous_frame, src2=prepared_frame)
+    previous_frame = prepared_frame
 
     # 4. Dilute the image a bit to make differences more seeable; more suitable for contour detection
-    #diff_frame = cv2.dilate(diff_frame, np.ones((5, 5)), 1)
-    # diff_frame = cv2.erode(diff_frame, np.ones((30, 30)), 1)
-    # diff_frame = cv2.morphologyEx(diff_frame, cv2.MORPH_CLOSE, kernel)
+    kernel = np.ones((5, 5))
+    diff_frame = cv2.dilate(diff_frame, kernel, 1)
 
     # 5. Only take different areas that are different enough (>20 / 255)
-    #thresh_frame = cv2.threshold(src=diff_frame, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(src=diff_frame, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
 
-    # 6a. Find and draw contours
-    #contours, _ = cv2.findContours(image=thresh_frame, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-    #cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+    # 6. Find and optionally draw contours
+    contours, _ = cv2.findContours(image=thresh_frame, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+    # Comment below to stop drawing contours
+    #cv2.drawContours(image=img_rgb, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+    # Uncomment 6 lines below to stop drawing rectangles
+    for contour in contours:
+      if cv2.contourArea(contour) < 50:
+        # too small: skip!
+          continue
+      (x, y, w, h) = cv2.boundingRect(contour)
+      cv2.rectangle(img=img_rgb, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
 
-    # 6b. Find and draw rectangles
-    #for contour in contours:
-    #  if cv2.contourArea(contour) < 50:
-    #    # too small: skip!
-    #      continue
-    #  (x, y, w, h) = cv2.boundingRect(contour)
-    #  cv2.rectangle(img=frame, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
+    cv2.imshow('Motion detector', img_rgb)
 
-    #cv2.imshow('thresh_frame', thresh_frame)
-    #cv2.imshow('diff_frame', diff_frame)
-    #cv2.imshow('Motion detector', frame)
-
-# press escape to exit
     if (cv2.waitKey(CAPTURE_DELAY_MS) == 27):
+      # out.release()
       break
 
 cap.release()
