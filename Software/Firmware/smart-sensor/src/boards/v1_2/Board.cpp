7d8a1d4db08b717c8831658280e2a3a58d1d0f8e
Maurice Snoeren <mac.snoeren@avans.nl>
2022-12-05 14:27:28 +0100

    Final merge

--- Software/Firmware/smart-sensor/src/boards/v1_2/Board.cpp
+++ Software/Firmware/smart-sensor/src/boards/v1_2/Board.cpp
@@ -87,14 +87,6 @@ void SmartSensorBoardV1_2::setup()
     _delay_ms(100);
     this->ledDriver->led1Off();
 
-<<<<<<< HEAD
-    if (this->isGateway())
-    {
-        this->ledDriver->led1Flash(5'000, 2'500);
-    }
-    else
-    {
-=======
     //this->checkGatewayAvailable();
     this->debug_P(PSTR("Gateway: "));
     if ( this->isGateway() ) {
@@ -102,7 +94,6 @@ void SmartSensorBoardV1_2::setup()
         this->ledDriver->led1Flash(5'000, 2'500);
     } else {
         this->debug_P(PSTR("No\n"));
->>>>>>> feature_wireless
         this->ledDriver->led1Flash(5'000, 100);
     }
 
@@ -113,11 +104,6 @@ void SmartSensorBoardV1_2::setup()
     this->serial0->setCallback(this);
 }
 
-<<<<<<< HEAD
-bool SmartSensorBoardV1_2::adapterInUse()
-{
-    return ((BOARDV1_2_ADAPTER_IN_USE_PORT & (1 << BOARDV1_2_ADAPTER_IN_USE_PIN)) != 0);
-=======
 bool SmartSensorBoardV1_2::checkGatewayAvailable() {
     char line[10] = "\0";
     this->serial0->print_P(PSTR("GWAV\n"));
@@ -130,7 +116,6 @@ bool SmartSensorBoardV1_2::checkGatewayAvailable() {
 
 bool SmartSensorBoardV1_2::adapterInUse() {
     return ( ( BOARDV1_2_ADAPTER_IN_USE_PORT & (1 << BOARDV1_2_ADAPTER_IN_USE_PIN) ) != 0 );
->>>>>>> feature_wireless
 }
 
 // TODO: When the
@@ -154,18 +139,10 @@ void SmartSensorBoardV1_2::debug_P(const char *message)
     this->serial0->print_P(message);
 }
 
-<<<<<<< HEAD
-const char *SmartSensorBoardV1_2::getID()
-{
-    // Get the Atmege unique serial number
-    for (uint8_t i = 0; i < 20; i = i + 2)
-    {
-=======
 void SmartSensorBoardV1_2::createID() {
     // Get the Atmege unique serial number
     // Create ID => SN<ATMEGA_ID>
     for ( uint8_t i=0; i < 20; i=i+2 ) {
->>>>>>> feature_wireless
         uint8_t b = boot_signature_byte_get(0x0E + i); // 0x0E => SER0
 
         uint8_t h1 = (b & 0b0000'1111);
